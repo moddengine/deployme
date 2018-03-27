9ac576294abc3da37adbd4808ad875bd76a93b86
Chris Seufert <chris@seufert.id.au>
2018-03-28 09:51:19 +1100

    generic index.php file

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?php
+# Update ModdEngine
+
+define("ME_SITE", "__SITEID__");
+define("ME_SITE_DIR", "__SITEDIR__");
+
+if(isset($_REQUEST['MODDENGINE_VERSION_OVERRIDE']) &&
+  preg_match('/^[a-z]$/', $_REQUEST['MODDENGINE_VERSION_OVERRIDE'])) {
+  $dir = realpath("../moddengine.{$_REQUEST['MODDENGINE_VERSION_OVERRIDE']}");
+  define("ME_DIR", "$dir");
+}
+if(!defined("ME_DIR"))
+  define("ME_DIR", "__MEDIR__");
+
+if(!is_file(ME_DIR."/www/index.php"))
+  die("<h2>ModdEngine Version does not exist on this server</h2>");
+
+require(ME_DIR."/www/index.php");
