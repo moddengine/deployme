7f4b06567738da90022a4a675c611b3db7039270
Chris Seufert <chris@seufert.id.au>
2018-03-28 12:07:52 +1100

    added ME_VER

--- template/index.php
+++ template/index.php
@@ -5,14 +5,17 @@ define("ME_SITE", "__SITEID__");
 define("ME_SITE_DIR", "__SITEDIR__");
 
 if(isset($_REQUEST['MODDENGINE_VERSION_OVERRIDE']) &&
-  preg_match('/^[a-z]$/', $_REQUEST['MODDENGINE_VERSION_OVERRIDE'])) {
-  $dir = realpath(__DIR__."/../moddengine.{$_REQUEST['MODDENGINE_VERSION_OVERRIDE']}");
+  preg_match('/^([a-z])$/', $_REQUEST['MODDENGINE_VERSION_OVERRIDE'], $m)) {
+  $dir = realpath(__DIR__ . "/../moddengine.{$m[1]}");
   define("ME_DIR", "$dir");
+  define("ME_VER", $m[1]);
 }
 if(!defined("ME_DIR"))
   define("ME_DIR", "__MEDIR__");
+if(!defined("ME_VER"))
+  define("ME_VER", "__MEVER__");
 
-if(!is_file(ME_DIR."/www/index.php"))
+if(!is_file(ME_DIR . "/www/index.php"))
   die("<h2>ModdEngine Version does not exist on this server</h2>");
 
-require(ME_DIR."/www/index.php");
+require(ME_DIR . "/www/index.php");
