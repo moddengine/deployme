cb80dcdbc67873a2fc080bafef386ef70d708db2
Chris Seufert <chris@seufert.id.au>
2018-04-09 15:44:33 +1000

    put full path on autoload

--- loader.php
+++ loader.php
@@ -2,5 +2,5 @@
 require_once(__DIR__."/vendor/autoload.php");
 
 $loader = new Aura\Autoload\Loader();
-$loader->addPrefix("ModdEngine\\Deploy", "/php/");
+$loader->addPrefix("ModdEngine\\Deploy", __DIR__."/php/");
 $loader->register();
